34e0406e05610a02096d407484897954be556c2f
Muzaffer <40807879+Muzo354510@users.noreply.github.com>
2026-03-05 16:10:54 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:10:49.623Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T13:10:55.743Z
+Commit: Fix memory leak in event handlers
