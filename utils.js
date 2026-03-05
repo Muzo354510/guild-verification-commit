6bce6fc49bdaa38f64d6ff45902e9c1c3c542f5b
Muzaffer <40807879+Muzo354510@users.noreply.github.com>
2026-03-05 16:12:49 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:12:49.439Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T13:12:50.657Z
+Commit: Patch API endpoint error handling
