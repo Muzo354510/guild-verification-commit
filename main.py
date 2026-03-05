d9564243a96065fc8a9fb67fd05ad96d7afe500b
Muzaffer <40807879+Muzo354510@users.noreply.github.com>
2026-03-05 16:10:56 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:10:51.040Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T13:10:57.087Z
+Commit: Fix CSS styling bug on mobile devices
