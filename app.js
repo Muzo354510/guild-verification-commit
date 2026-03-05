aa2ef0bfac75dd418138b0eef25b5b2ff7036f08
Muzaffer <40807879+Muzo354510@users.noreply.github.com>
2026-03-05 16:12:45 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:12:37.256Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T13:12:46.890Z
+Commit: Fix broken links in documentation
