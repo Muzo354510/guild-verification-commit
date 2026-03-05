b2016d6c92682b25d9f27734e5b49e1da10c0bde
Muzaffer <40807879+Muzo354510@users.noreply.github.com>
2026-03-05 16:12:52 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:12:50.657Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T13:12:53.223Z
+Commit: Fix broken links in documentation
